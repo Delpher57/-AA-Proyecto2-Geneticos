149d41e8ad1e52468bfeb4e2c5d1018fc63ed316
Oswaldo <oswaldo@estudiantec.cr>
2021-06-08 21:29:19 -0600

    poblacion inicial y calculos de porcentage

--- paint/paint.py
+++ paint/paint.py
@@ -14,6 +14,9 @@ COLOR_3 = pygame.Color("#57cc99")
 COLOR_4 = pygame.Color("#38a3a5")
 COLOR_5 = pygame.Color("#22577a")
 
+#constantes:
+TREE_POS_X = 955
+TREE_POS_Y = 359
 
 
 pygame.init()
@@ -38,6 +41,8 @@ menu_rect = pygame.Rect(0, 0, 150, 360)
 
 tree_rect = pygame.Rect(640, 0, 640, 720)
 tree_drawing_canvas_rect = pygame.Rect(795, 20, 320, 360)
+
+porcentaje_rect = pygame.Rect(27, 600, 90, 200)
 
 left_scr_rect = pygame.Rect(0, 0, 640, 720)
 drawing_canvas_rect = pygame.Rect(160, 20, 320, 360)
@@ -86,12 +91,97 @@ def get_diferencia(dibujo,arbol):
         img = pygame.image.fromstring(diff.tobytes(), diff.size, diff.mode)
 
         img = pygame.transform.scale(img, (220,248))
+        porcentaje = get_black_percentage((220,248),img)
+        porcentaje_txt = menu_font.render("{:.5f}".format(porcentaje) + "%", True, COLOR_1)
+        pygame.draw.rect(screen, COLOR_3, porcentaje_rect)
+        screen.blit(porcentaje_txt, (27, 600))
         screen.blit(img, (794, 400))
 
 
 # ------------------------------------- #
+def modificar_parametro(parametro,mutacion=0):
+    """[recibimos un parametro y lo retornamos mutado (positivo para evitar cosas raras)]
+
+    Args:
+        parametro ([int]): [parametro que vamos a mutar]
+        mutacion (int, optional): [cantidad de mutacion que hay, mas alto mas deberia cambiar]. Defaults to 0.
+
+    Returns:
+        [type]: [description]
+    """    
+    multiplier = random.choice([-1,1]) #hacemos negativo o positivo
+    modificador = random.randint(0,mutacion)
+    parametro += modificador*multiplier
+    return abs(parametro)
+
+def get_black_percentage(size,img):
+    color = pygame.Color((0, 0, 0, 255))
+
+    width = size[0]
+    height = size[1]
+    total_number_of_pixels = width * height
+    number_of_pixels = 0
+
+    for x in range(0, width):
+        for y in range(0, height):
+            #print(img.get_at((x, y)))
+            if img.get_at((x, y)) == color:
+                number_of_pixels += 1
+
+    percentage = number_of_pixels / total_number_of_pixels * 100
+    return percentage
+
+def get_parametros_random():
+    """[lo usamos para obtener parametros para la poblacion inicial]
+
+    Returns:
+        [array]: [lista con los parametros]
+    """    
+    profundidad = random.randint(3,7)
+    tamanno = random.randint(10,15)
+    cantidad_ramas = random.randint(2,6)
+    ancho_tronco=8 #esto lo hacemos constante por que siempre va a ser igual al dibujo
+    random1 = random.randint(1,20) #el de angulos
+    random2 = random.randint(1,10) #este no puede ser tan grande o se hace algo rarisimo
+
+    if profundidad > 5:
+        tamanno = random.randint(5,8)
+        cantidad_ramas = random.randint(2,4)
 
 
+
+    return [profundidad,tamanno,cantidad_ramas,ancho_tronco,random1,random2]
+    #tree(x1=TREE_POS_X,y1=TREE_POS_Y,profundidad=5,tamanno=10,cantidad_ramas=3,ancho_tronco=8,random1=5,random2=5)
+
+
+
+def get_poblacion_inicial(cantidad=8):
+    """[obtenemos la poblacion inicial]
+
+    Args:
+        cantidad (int, optional): [cantidad de individuos de la poblacion inicial]. Defaults to 8.
+
+    Returns:
+        [array]: [lista con las imagenes (en formato pygame) de la poblacion inicial]
+    """    
+    img1 = get_draw_image()
+    poblacion_inicial = []
+    for i in range(0,cantidad):
+        parametros = get_parametros_random() #obtenemos los parametros
+
+        clean_tree_canvas()
+        
+        tree(x1=TREE_POS_X,y1=TREE_POS_Y,profundidad=parametros[0],tamanno=parametros[1],cantidad_ramas=parametros[2],ancho_tronco=parametros[3],random1=parametros[4],random2=parametros[5])
+        pygame.display.update()
+        img2 = get_tree_image()
+        poblacion_inicial += [img2]
+        get_diferencia(img1,img2)
+    
+    return poblacion_inicial
+
+
+
+#----------------------------------------#
 
 def get_random(range=1):
     return random.uniform(-range, range) * random.choice([-1,1])
@@ -102,6 +192,21 @@ def reduce_num(num):
     else:
         return num - 0.75
 
+
+def get_tree_image():
+    """[guardamos una imagen del arbl generado]
+
+    Returns:
+        [imagen/surface]: [retornamos la variable con la imagen]
+    """    
+    save_surface_tree = pygame.Surface((320, 360))
+    save_surface_tree.blit(screen, (0, 0), (795, 20, 320, 360))
+    return save_surface_tree
+
+def get_draw_image():
+    save_surface_draw = pygame.Surface((320, 360))
+    save_surface_draw.blit(screen, (0, 0), (160, 20, 320, 360))
+    return save_surface_draw
 
 def drawTree(x1, y1, angle, depth, angulo_inicio, tamanno,cantidad_ramas=4,grueso=2, random=0,random2=0):
     
@@ -162,6 +267,7 @@ def clean_draw_canvas():
 
 def clean_tree_canvas():
     #pygame.draw.rect(screen, COLOR_2, tree_rect)
+    pygame.draw.rect(screen, COLOR_2, tree_rect)
     pygame.draw.rect(screen, COLOR_4, tree_drawing_canvas_rect, 5)
     pygame.draw.rect(screen, COLOR_1, tree_drawing_canvas_rect)
 
@@ -224,7 +330,7 @@ while True:
 
     pygame.draw.rect(screen, COLOR_4, save_rect)
     screen.blit(save_text, (30, 262))
-
+    
 
 
 
@@ -232,20 +338,23 @@ while True:
     #detectamos si se hizo click en guardar
     if draw == True and save_flag == False:
         if save_rect.collidepoint(mouse_pos):
+            get_poblacion_inicial(1)
+            save_flag = True
+            """
             print("File has been saved :P")
             
             clean_tree_canvas()
-            tree(x1=955,y1=359,profundidad=5,tamanno=10,cantidad_ramas=3,ancho_tronco=8,random1=5,random2=5)
+            tree(x1=TREE_POS_X,y1=TREE_POS_Y,profundidad=5,tamanno=10,cantidad_ramas=3,ancho_tronco=8,random1=5,random2=5)
 
             save_surface_draw = pygame.Surface((320, 360))
             save_surface_draw.blit(screen, (0, 0), (160, 20, 320, 360))
 
-            #tree_drawing_canvas_rect = pygame.Rect(795, 20, 320, 360)
+            #guardamos el arbol
             save_surface_tree = pygame.Surface((320, 360))
             save_surface_tree.blit(screen, (0, 0), (795, 20, 320, 360))
             get_diferencia(save_surface_draw,save_surface_tree)
             save_flag = True
-
+            """
             
         
 
